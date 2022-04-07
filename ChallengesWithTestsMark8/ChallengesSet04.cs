cce0a3aaa55ef990ebd48e472a5e1e0d2c2997bd
Hayden Faw <faw.hayden@gmail.com>
2022-04-06 22:48:46 -0500

    completed ChallengeSet04

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,52 +1,81 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet04
     {
-        public int AddEvenSubtractOdd(int[] numbers)
+        public int AddEvenSubtractOdd(int[] numbers) //done
         {
-            throw new NotImplementedException();
+            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
+            //throw new NotImplementedException();
         }
 
-        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
+        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4) //done
         {
-            throw new NotImplementedException();
+
+            int[] countOfStrings = {str1.Count(), str2.Count(), str3.Count(), str4.Count()};
+            return countOfStrings.Min();
+            //throw new NotImplementedException();
         }
 
-        public int GetSmallestNumber(int number1, int number2, int number3, int number4)
+        public int GetSmallestNumber(int number1, int number2, int number3, int number4) //done
         {
-            throw new NotImplementedException();
+            int[] arrayOfNumbers = {number1, number2, number3, number4};
+            return arrayOfNumbers.Min();
+            //throw new NotImplementedException();
         }
 
-        public void ChangeBusinessNameTo_TrueCoders(Business biz)
+        public void ChangeBusinessNameTo_TrueCoders(Business biz) //done
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
+            //throw new NotImplementedException();
         }
 
-        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
+        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3) // done
         {
-            throw new NotImplementedException();
+            return ((sideLength1 + sideLength2 > sideLength3) && (sideLength1 + sideLength3 > sideLength2) && (sideLength2 + sideLength3 > sideLength1)) ? true : false;
+            //throw new NotImplementedException();         
         }
 
-        public bool IsStringANumber(string input)
+        public bool IsStringANumber(string input) //done
         {
-            throw new NotImplementedException();
+            return (double.TryParse(input, out double i)) ? true : false;
+            //throw new NotImplementedException();
         }
 
-        public bool MajorityOfElementsInArrayAreNull(object[] objs)
+        public bool MajorityOfElementsInArrayAreNull(object[] objs) //done
         {
-            throw new NotImplementedException();
+            return ((objs.Where(x => x == null).Count()) > (objs.Where(x => x != null)).Count()) ? true : false;
+            //throw new NotImplementedException();
         }
 
-        public double AverageEvens(int[] numbers)
+        public double AverageEvens(int[] numbers) //done
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            if (numbers.Where(x => x%2 ==0).Sum() == 0)
+            {
+                return 0;
+            }
+            return numbers.Where(x => x % 2 == 0).Average();
+            //throw new NotImplementedException();
         }
 
-        public int Factorial(int number)
+        public int Factorial(int number) //done
         {
-            throw new NotImplementedException();
+            if(number ==0)
+            {
+                return 1;
+            }
+            for (int i = number - 1; i >= 1; i--)
+            {
+                number = number * i;
+            }
+            return (number >= 0) ? number : throw new ArgumentOutOfRangeException();
+            //throw new NotImplementedException();
         }
     }
 }
